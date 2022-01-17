7387899e375b544005a024c3828d866c1b81fa23
Austin Cory Bart <acbart@vt.edu>
2022-01-17 18:44:07 -0500

    Provide Bootstrap as if imported

--- docs/assets/js/runnable_ts.js
+++ docs/assets/js/runnable_ts.js
@@ -1,4 +1,21 @@
 ({useState, useEffect} = React);
+({Accordion, AccordionButton, AccordionCollapse, AccordionContext, 
+  Alert, Anchor, Badge, Breadcrumb, BreadcrumbItem, Button, 
+  ButtonGroup, ButtonToolbar, Card, CardGroup, CardImg, 
+  Carousel, CarouselItem, CloseButton, Col, Collapse, Container, 
+  Dropdown, DropdownButton, Fade, Figure, FloatingLabel, Form, 
+  FormCheck, FormControl, FormFloating, FormGroup, FormLabel, 
+  FormSelect, FormText, Image, InputGroup, ListGroup, ListGroupItem, 
+  Modal, ModalBody, ModalDialog, ModalFooter, ModalTitle, 
+  Nav, NavDropdown, NavItem, NavLink, Navbar, NavbarBrand, 
+  Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle, Overlay, 
+  OverlayTrigger, PageItem, Pagination, Placeholder, PlaceholderButton, 
+  Popover, PopoverBody, PopoverHeader, ProgressBar, Ratio, Row, 
+  SSRProvider, Spinner, SplitButton, Stack, Tab, TabContainer, 
+  TabContent, TabPane, Table, Tabs, ThemeProvider, Toast, ToastBody, 
+  ToastContainer, ToastHeader, ToggleButton, ToggleButtonGroup, 
+  Tooltip, useAccordionButton} = ReactBootstrap);
+
 exports = {};
 
 function _getHtmlSelector(el) {
